1981bc206440f8871819c7beb9380bf82ac3d50b
Patrik Behul <behulpatrik@gmail.com>
2026-06-24 12:00:40 +0200

    Created Basic Project Structure.

---
+++ src/ts/dist/index.js
@@ -0,0 +1,5 @@
+const game = document.querySelector(".game"); // Gets The Game Canvas
+const game_ctx = game.getContext("2d"); // Gets The Game CTX
+console.log(game_ctx);
+export {};
+//# sourceMappingURL=index.js.map
---
+++ src/ts/src/index.ts
@@ -0,0 +1,4 @@
+const game:HTMLCanvasElement = document.querySelector(".game") as HTMLCanvasElement // Gets The Game Canvas
+const game_ctx = game.getContext("2d") // Gets The Game CTX
+
+console.log(game_ctx)
